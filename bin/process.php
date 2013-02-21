02a47b491686b59abd335401509f7235034d9f68
Peter Ainsworth <pete@spirelightmedia.com>
2013-02-21 18:29:20 +0000

    Added code to send email from the To field

--- bin/process.php
+++ bin/process.php
@@ -1,14 +1,14 @@
 <?php
-$mail=$_POST['mail'];
-$name=$_POST['firstName']." ".$_POST['lastName'];
-$subject=$_POST['company'];
-$text=$_POST['zip']." ".$_POST['phone'];
-$to="pete@spirelightmedia.com";
-$message=" You received  a mail from ".$mail;
-$message.=" Text of the message : ".$text;
-if(mail($to,$subject,$message)) {
-echo "mail successful send";
+$mail = $_POST['mail'];
+$name = $_POST['firstName'] . " " . $_POST['lastName'];
+$subject = $_POST['company'];
+$text = $_POST['message'];
+$to = urldecode($_POST['recipient']);
+$message = " You received  a mail from " . $mail;
+$message .= " Text of the message : " . urldecode($text);
+if (mail($to, $subject, $message)) {
+	echo "mail successful send";
 } else {
-echo "there’s some errors to send the mail, verify your server options";
+	echo "there’s some errors to send the mail, verify your server options";
 }
 ?>
