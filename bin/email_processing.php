7966317a6e1aebf556ac77c40cdfed32fc01810d
Peter Ainsworth <pete@spirelightmedia.com>
2013-03-19 15:30:42 +0000

    Removed $availableSpouts array from this file and reconstructed it from the settings section of the HTML page. Added an abs(0 to the nearestpout calculations. Converted spout size calculation to work dynamically from an process all spout fields that have been set to results in a spout size calculation for each field. Added a reset for the spout fields on successful submission.

--- bin/email_processing.php
+++ bin/email_processing.php
@@ -3,48 +3,67 @@
 require_once 'swiftmailer/lib/swift_required.php';
 
 if (isset($_POST['nojs'])) {
-	$availableSpouts = array(0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5);
-	function nearestSpout($availableSpouts, $containerDiameter) {
+	// Retreive the list of available spouts from the HTML
+	$availableSpouts = preg_replace('/\s+/', '', $spoutSizes);
+	$availableSpouts = explode(",", $availableSpouts);
+	// Calculate the nearest available spout according to the calculated spout size
+	function nearestSpout($containerDiameter, $availableSpouts) {
 		$closest = null;
 		$calculatedSpoutSize = $containerDiameter * 0.72;
-		foreach($availableSpouts as &$value) {
-			if ($closest == null || $value - $calculatedSpoutSize < $closest - $calculatedSpoutSize) {
+		foreach ($availableSpouts as $value) {
+			if ($closest == null
+					|| abs($value - $calculatedSpoutSize)
+					< abs($closest - $calculatedSpoutSize)) {
 				$closest = $value;
 			}
-		};
+		}
+		;
 		return $closest;
 	}
-	$weightHopperSize = explode("-", $weighhopper);
+	// Process the correct dimension fields according to the spout type selection
+	$spoutRows = "";
+	$index = 0;
+	foreach ($spoutFields as $key => $value) {
+		$index++;
+		$spoutID = "Spout" . $index;
+		$spoutwidth = "width" . $spoutID . "Fallback";
+		$spoutd1 = "d1" . $spoutID . "Fallback";
+		$spoutd2 = "d2" . $spoutID . "Fallback";
+		$spoutdiameter = "diameter" . $spoutID . "Fallback";
+		if (!empty($value)) {
+			switch ($value) {
+			case 'flat-bag':
+				$containerDiameter = ${$spoutwidth} * 2 / M_PI;
+				$spoutSize = nearestSpout($containerDiameter, $availableSpouts);
+				break;
+			case 'four-sided-bag':
+				$containerDiameter = (${$spoutd1} + ${$spoutd2}) * 2 / M_PI;
+				$spoutSize = nearestSpout($containerDiameter, $availableSpouts);
+				break;
+			case 'can-jar':
+				foreach ($availableSpouts as $value) {
+					if ($spoutSize == null
+							|| ${$spoutdiameter} - $value >= 0.125) {
+						$spoutSize = $value;
+					}
+				}
+				;
+				break;
+			}
+			$spoutRows .= "<tr><td>Spout</td><td>" . $spoutSize . " inch"
+					. "</td><td>" . $settings["spout"]["price"] . "</td></tr>";
+		}
+	}
+	// Determine the weigh hopper size and discharge funnel type in order to list the correct name, description and price for the dischgarge funnel on the order summar
+	$weighHopperSize = explode("-", $weighhopper);
 	$dischargeFunnelType = explode("-", $dischargefunnel);
+	// Format the message or set the variable as empty if not set
 	if (!empty($message)) {
 		$message = urldecode($message) . "\n\r";
 	} else {
 		$message = "";
 	}
-	if (!empty($typeSpout1Default)) {
-		switch ($typeSpout1Default) {
-			case 'flat-bag':
-				$containerDiameter = $widthInchesSpout1Default * 2 / M_PI;
-				$spoutSize = nearestSpout($availableSpouts, $containerDiameter);
-				break;
-			case 'four-sided-bag':
-				$containerDiameter = ($d1InchesSpout1Default + $d2InchesSpout1Default) * 2 / M_PI;
-				$spoutSize = nearestSpout($containerDiameter);
-				break;
-			case 'can-jar':
-				foreach($availableSpouts as &$value) {
-					if ($spoutSize == null || $diameterInchesSpout1Default - $value >= 0.125) {
-						$spoutSize = $value;
-					}
-				};
-				break;
-		}
-		$spoutRow1 = "<tr><td>Spout</td><td>"
-			. $spoutSize . " inch"
-			. "</td><td>"
-			. $settings["spout-price"]
-			. "</td></tr>";
-		}
+	// Create the HTML message
 	$messageHTML = $message
 			. "<h3>Your Quote Summary</h3><table><thead><tr><th>Item</th><th>Description</th><th>Price</th></tr></thead><tbody><tr><td>"
 			. $settings["machinemodel"][$machinemodel]["name"] . " "
@@ -56,12 +75,12 @@ if (isset($_POST['nojs'])) {
 			. $settings["weighhopper"][$weighhopper]["description"]
 			. "</td><td>" . $settings["weighhopper"][$weighhopper]["price"]
 			. "</td></tr><tr><td>"
-			. $settings["dischargefunnel"][$weightHopperSize[0]][$dischargeFunnelType[0]]["name"]
+			. $settings["dischargefunnel"][$weighHopperSize[0]][$dischargeFunnelType[0]]["name"]
 			. "</td><td>"
-			. $settings["dischargefunnel"][$weightHopperSize[0]][$dischargeFunnelType[0]]["description"]
+			. $settings["dischargefunnel"][$weighHopperSize[0]][$dischargeFunnelType[0]]["description"]
 			. "</td><td>"
-			. $settings["dischargefunnel"][$weightHopperSize[0]][$dischargeFunnelType[0]]["price"]
-			. "</td></tr>" . $spoutRow1 . "</tbody></table>";
+			. $settings["dischargefunnel"][$weighHopperSize[0]][$dischargeFunnelType[0]]["price"]
+			. "</td></tr>" . $spoutRows . "</tbody></table>";
 
 	$messageText = "Text of the message : " . urldecode($message)
 			. "Your Quote Summary \r"
@@ -107,7 +126,12 @@ if (!empty($cc)) {
 }
 
 if ($mailer->send($emailMessage)) {
-	$response = "<div class=\"success\"><p>Thank you. Your email has been successfully sent.</p></div>";
+	//header('Location: index.php');
+	unset($_POST['typeSpout1Fallback'], $_POST['typeSpout2Fallback'], $_POST['typeSpout3Fallback']);
+		$_POST['typeSpout1Fallback'] = "";
+		$_POST['typeSpout2Fallback'] = "";
+		$_POST['typeSpout3Fallback'] = "";
+	$response = "<div class=\"success\"><p>Thank you. Your email has been successfully sent.</p></div>" . $messageHTML;
 } else {
 	$errors['mailfail'] = true;
 }
