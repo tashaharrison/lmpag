321440ca1d5b7ecc1f40a27cb5300af8b178cef5
Peter Ainsworth <pete@spirelightmedia.com>
2013-03-20 15:34:31 +0000

    Added styling to the HTML email and commented out logo attachment for the time being.

--- bin/email_processing.php
+++ bin/email_processing.php
@@ -135,29 +135,206 @@ $transport = Swift_MailTransport::newInstance();
 $mailer = Swift_Mailer::newInstance($transport);
 // Create a message
 $emailMessage = Swift_Message::newInstance('Logical Machines Quote Generator');
-$cid = $emailMessage
-		->embed(
-				Swift_Image::fromPath(
-						'http://www.logicalmachines.com/Resources/lmweblogo.jpeg'));
+/*$cid = $emailMessage
+        ->embed(
+                Swift_Image::fromPath(
+                        'http://www.logicalmachines.com/Resources/lmweblogo.jpeg'));*/
 $emailMessage
 		->setBody(
-				'<html>' . ' <head></head>'
-						. ' <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">'
+				'<html>'
+						. ' <head>	<style type="text/css">
+		#outlook a{
+			padding:0;
+		}
+		body{
+			width:100% !important;
+		}
+		body{
+			-webkit-text-size-adjust:none;
+		}
+		body{
+			margin-bottom:0;
+			margin-left:0;
+			margin-right:0;
+			margin-top:10px;				
+			padding:0;
+		}
+		img{
+			border:0;
+			height:auto;
+			line-height:100%;
+			outline:none;
+			text-decoration:none;
+		}
+		table td{
+			border-collapse:collapse;
+		}
+		#backgroundTable{
+			height:100% !important;
+			margin:0;
+			padding:0;
+			width:100% !important;
+		}
+			body,#backgroundTable{
+			background-color:#FAFAFA;
+		}
+		#templateContainer{
+			border:1px solid #DDDDDD;
+		}
+		h1,.h1{
+			color:#202020;
+			display:block;
+			font-family:Arial;
+			font-size:34px;
+			font-weight:bold;
+			line-height:100%;
+			margin-top:0;
+			margin-right:0;
+			margin-bottom:10px;
+			margin-left:0;
+			text-align:left;
+		}
+		h2,.h2{
+			color:#202020;
+			display:block;
+			font-family:Arial;
+			font-size:30px;
+			font-weight:bold;
+			line-height:100%;
+			margin-top:0;
+			margin-right:0;
+			margin-bottom:10px;
+			margin-left:0;
+			text-align:left;
+		}
+		h3,.h3{
+			color:#202020;
+			display:block;
+			font-family:Arial;
+			font-size:26px;
+			font-weight:bold;
+			line-height:100%;
+			margin-top:0;
+			margin-right:0;
+			margin-bottom:10px;
+			margin-left:0;
+			text-align:left;
+		}
+		h4,.h4{
+			color:#202020;
+			display:block;
+			font-family:Arial;
+			font-size:22px;
+			font-weight:bold;
+			line-height:100%;
+			margin-top:0;
+			margin-right:0;
+			margin-bottom:10px;
+			margin-left:0;
+			text-align:left;
+		}
+		#templatePreheader{
+			background-color:#FAFAFA;
+		}
+		.preheaderContent div{
+			color:#505050;
+			font-family:Arial;
+			font-size:10px;
+			line-height:100%;
+			text-align:left;
+		}
+		.preheaderContent div a:link,.preheaderContent div a:visited,.preheaderContent div a .yshortcuts {
+			color:#336699;
+			font-weight:normal;
+			text-decoration:underline;
+		}
+		#templateHeader{
+			background-color:#FFFFFF;
+			border-bottom:0;
+		}
+		.headerContent{
+			color:#202020;
+			font-family:Arial;
+			font-size:34px;
+			font-weight:bold;
+			line-height:100%;
+			padding:0;
+			text-align:center;
+			vertical-align:middle;
+		}
+		.headerContent a:link,.headerContent a:visited,.headerContent a .yshortcuts {
+			color:#336699;
+			font-weight:normal;
+			text-decoration:underline;
+		}
+		#headerImage{
+			height:auto;
+			max-width:600px !important;
+		}
+		#templateContainer,.bodyContent{
+			background-color:#FFFFFF;
+		}
+		.bodyContent div{
+			color:#505050;
+			font-family:Arial;
+			font-size:14px;
+			line-height:150%;
+			text-align:left;
+		}
+		.bodyContent div a:link,.bodyContent div a:visited,.bodyContent div a .yshortcuts {
+			color:#336699;
+			font-weight:normal;
+			text-decoration:underline;
+		}
+		.bodyContent img{
+			display:inline;
+			height:auto;
+		}
+		#templateFooter{
+			background-color:#FFFFFF;
+			border-top:0;
+		}
+		.footerContent div{
+			color:#707070;
+			font-family:Arial;
+			font-size:12px;
+			line-height:125%;
+			text-align:left;
+		}
+		.footerContent div a:link,.footerContent div a:visited,.footerContent div a .yshortcuts {
+			color:#336699;
+			font-weight:normal;
+			text-decoration:underline;
+		}
+		.footerContent img{
+			display:inline;
+		}
+				</style></head>'
+						. ' <body leftmargin="0" marginwidth="0" topmargin="10" marginheight="0" offset="0">'
 						. '<center>'
+				// Background container
 						. '<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="backgroundTable">'
 						. '<tr><td align="center" valign="top">'
-						. '<table border="0" cellpadding="0" cellspacing="0" width="100%" id="templateContainer">'
+				// Email container
+						. '<table border="0" cellpadding="0" cellspacing="0" width="600" id="templateContainer">'
 						. '<tr><td align="center" valign="top">'
-						. '<table border="0" cellpadding="0" cellspacing="0" width="100%" id="templateHeader">'
-						. '<tr><td class="headerContent">' . '<img src="'
-						. $cid . '" alt="Logical Machines" />' . '</td></tr>'
-						. '</table>'
-						. '</td></tr><tr><td align="center" valign="top">'
-						. '<table border="0" cellpadding="0" cellspacing="0" width="100%" id="templateBody">'
+				// Header container
+						. '<table border="0" cellpadding="0" cellspacing="0" width="600" id="templateHeader">'
+						. '<tr><td class="headerContent">' /*. '<img src="' . $cid . '" alt="Logical Machines" />'*/
+ . '</td></tr>' . '</table>' . '</td></tr><tr><td align="center" valign="top">'
+				// Body container
+						. '<table border="0" cellpadding="0" cellspacing="0" width="600" id="templateBody">'
 						. '<tr><td valign="top" class="bodyContent">'
 						. '<table border="0" cellpadding="20" cellspacing="0" width="100%">'
 						. '<tr><td valign="top">' . $messageHTML . '</td></tr>'
 						. '</table></td></tr></table>' . '</td></tr>'
+						. '<tr><td align="center" valign="top">'
+				// Footer container
+						. '<table border="0" cellpadding="10" cellspacing="0" width="100%" id="templateFooter">'
+						. '<tr><td valign="top" class="footerContent">'
+						. '<table border="0" cellpadding="10" cellspacing="0" width="100%">'
+						. '<tr><td colspan="2" valign="middle"></td></tr>'
+						. '</table>' . '</td></tr>' . '</table>' . '</td></tr>'
 						. '</table>' . '<br></td></tr>' . '</table></center>'
 						. '</body>' . '</html>', 'text/html');
 $emailMessage->addPart($messageText, 'text/plain');
